ea45a94f99ab2ad94af87ae5f1b5da00288116b7
Pooja Raval <poojaraval2130@gmail.com>
2019-09-05 17:13:36 +0530

    Created Nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './tapodhan.jpeg';
+import './App.css'; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+  
+   
+  <div className="App"> 
+  <header className="App-header">
+  
+  <img src={logo} alt="logo"></img>
+  Welcome to Tapodhan Brahmin Samaj 12 Gaam 
+    <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.22gamtapodhanbrahmansamaj.com/material/memberform.pdf"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go To Registration Form
         </a>
-      </header>
+        </header>
     </div>
   );
 }
